85a94dc0d205779feda91e49f0944a1c085ba8d4
JirenJerry <riverderio@gmail.com>
2021-06-13 14:23:47 -0600

    Se agrega un archivo para cosas de firebase

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -1,18 +1,5 @@
 
-// Your web app's Firebase configuration
-let firebaseConfig = {
-  apiKey: "AIzaSyAKujnQ66H5SuApJ8bv7VcEC1PjypgoLmQ",
-  authDomain: "dbaquametrics.firebaseapp.com",
-  databaseURL: "https://dbaquametrics-default-rtdb.firebaseio.com",
-  projectId: "dbaquametrics",
-  storageBucket: "dbaquametrics.appspot.com",
-  messagingSenderId: "534745976249",
-  appId: "1:534745976249:web:b6575d822166e80f5c7a68"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
 
-let db = firebase.firestore();
 let btnSignIn = document.querySelector("#btnSignIn");
 let h1UserName = document.querySelector("#h1UserName");
 let btnLogin = document.querySelector("#loginBtn");
