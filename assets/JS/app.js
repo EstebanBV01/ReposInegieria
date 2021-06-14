55d91c2a00f9e01b07c5906bac58d3ebcfa55abe
JirenJerry <riverderio@gmail.com>
2021-06-13 21:53:54 -0600

    Añadido de un poco de mierdas

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -2,12 +2,20 @@ let bntGoogle = document.querySelector("#btnGoogle");
 let h1UserName = document.querySelector("#h1UserName");
 let btnLogin = document.querySelector("#loginBtn");
 let btnRegistro = document.querySelector("#registroBtn")
-let modal = document.querySelector("#exampleModalLabel");
+let modal = document.querySelector("#signUpModal");
+let labelImage = document.querySelector("#labelLogin");
+
 const btnClick = (btnOn, btnOff) => {
     btnOn.classList.add("btnOnClick");
     btnOff.classList.remove("btnOnClick");
 
 }
+
+labelImage.addEventListener('click', async() => {
+    if (!firebase.auth().currentUser) {
+        $('#signUpModal').modal('show')
+    }
+})
 
 btnLogin.addEventListener("click", () => {
     btnClick(btnLogin, btnRegistro);
@@ -17,13 +25,23 @@ btnRegistro.addEventListener("click", () => {
     btnClick(btnRegistro, btnLogin);
 })
 
+
+
 bntGoogle.addEventListener('click', async() => {
+
+
+
+
     let user = await registerUserGoogle();
     h1UserName.innerHTML = user.displayName;
-    await validateWaterMeter("bWV33WRvcjE=");
-    console.log(user.uid);
+
+    console.log(user.photoURL);
 
     console.log("DONE!");
-    $('#exampleModalLabel').modal('hide');
+    $('#signUpModal').modal('hide')
+    $("#labelLoginImage").toggleClass("fas fa-user fas fa-sign-out-alt fa-lg");
+    $("#labelLoginTexto").html("Salir");
+
+
 
 })
