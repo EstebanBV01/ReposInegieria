1a18e292e25cd0f33cea7b97889e051e95f00bef
JirenJerry <riverderio@gmail.com>
2021-06-13 22:58:51 -0600

    Login con foto de usuario

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -12,9 +12,20 @@ const btnClick = (btnOn, btnOff) => {
 }
 
 labelImage.addEventListener('click', async() => {
-    if (!firebase.auth().currentUser) {
+    let user = firebase.auth().currentUser
+    console.log(user);
+
+    if (!user) {
         $('#signUpModal').modal('show')
+    } else {
+        console.log("false");
+        firebase.auth().signOut().then(() => {
+            // Sign-out successful.
+        }).catch((error) => {
+            // An error happened.
+        });
     }
+
 })
 
 btnLogin.addEventListener("click", () => {
@@ -25,23 +36,41 @@ btnRegistro.addEventListener("click", () => {
     btnClick(btnRegistro, btnLogin);
 })
 
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+
+        loggedIn(user);
+
+    } else {
+        loggedOut();
+    }
+});
+const loggedOut = () => {
+    $("#labelLoginImage").toggleClass("fas fa-user fas fa-sign-out-alt fa-lg");
+    $("#labelLoginTexto").html("Login or SignUp");
+    h1UserName.innerHTML = "Nadie esta logueado";
+    $("#userProfileLi").remove();
+}
+const loggedIn = (user) => {
+    console.log("logged in !!");
+    console.log({ user });
+    $("#labelLoginImage").toggleClass("fas fa-user fas fa-sign-out-alt fa-lg");
+    $("#labelLoginTexto").html("Salir");
+    let picture = document.createElement('img');
+    let list = document.createElement("li");
+    list.id = "userProfileLi";
+    list.classList.add("nav-item");
+    picture.src = user.photoURL;
+    picture.classList.add("rounded-circle");
+    h1UserName.innerHTML = user.displayName;
+    picture.id = "userPicture";
+    list.append(picture);
+    $("#listaNavBar").prepend(list);
+}
 
 
 bntGoogle.addEventListener('click', async() => {
-
-
-
-
-    let user = await registerUserGoogle();
-    h1UserName.innerHTML = user.displayName;
-
-    console.log(user.photoURL);
-
+    await registerUserGoogle();
     console.log("DONE!");
     $('#signUpModal').modal('hide')
-    $("#labelLoginImage").toggleClass("fas fa-user fas fa-sign-out-alt fa-lg");
-    $("#labelLoginTexto").html("Salir");
-
-
-
 })
